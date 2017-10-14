8ceb04a3c4f31670ac3b7930b494e1746ffee52a
Harshil Shah <harshilshah1910@outlook.com>
2017-10-14 22:35:42 +0530

    Added a basic example implementation

--- Example/Sources/ViewController.swift
+++ Example/Sources/ViewController.swift
@@ -10,15 +10,35 @@ import UIKit
 import ActivityRings
 
 class ViewController: UIViewController {
+    
+    private lazy var activityRingView = ActivityRingView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .black
+        
+        activityRingView.progress = 0.75
+        activityRingView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(activityRingView)
+        NSLayoutConstraint.activate([
+            activityRingView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityRingView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityRingView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            activityRingView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.9),
+        ])
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.activityRingView.animateProgress(to: 1.5, withDuration: 2)
+        }
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
 }
