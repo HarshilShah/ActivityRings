d288e312515a364cb0d3a20328e9fad6b1cafff4
Harshil Shah <harshilshah1910@outlook.com>
2017-10-14 22:14:30 +0530

    Inlined the ProgressShader to work around some compile issues

--- Sources/ActivityRingScene.swift
+++ Sources/ActivityRingScene.swift
@@ -91,7 +91,22 @@ final class ActivityRingScene: SKScene {
         shadowShapeNode.strokeColor = .black
         shadowNode.addChild(shadowShapeNode)
         
-        let ringShader = SKShader(fileNamed: "ProgressShader.fsh")
+        /// This shader simply sets the color at any point to be a mixture of the
+        /// `start_color` and `end_colors` in the same proportion as it's distance from
+        /// each end
+        ///
+        /// The `end_color` represents the color at the end of a full revolution,
+        /// so for cases when the progress is under a full revolution (i.e. <1), the
+        /// actual line never is of that color. So a `progress` uniform is used to
+        /// adjust the proportional distance of a point from the end of a full
+        /// revolution
+        let ringShader = SKShader(source: """
+            void main() {
+                float ratio = progress * v_path_distance / u_path_length;
+                gl_FragColor = vec4((start_color * (1.0 - ratio)) + (end_color * ratio), 1.0);
+            }
+        """)
+        
         gradientArcNode.strokeShader = ringShader
         gradientArcNode.lineCap = .round
         addChild(gradientArcNode)
