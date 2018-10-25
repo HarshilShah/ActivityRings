c9d3d4205ba33eeaaf4f39c19437270e6e0aa1ff
Cal Stephens <cal@calstephens.tech>
2018-10-25 08:16:25 -0400

    Add a watchOS example app

---
+++ Examples/watchOS Example/ActivityRings watchOS Example WatchKit Extension/ExtensionDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.swift
+//  ActivityRings watchOS Example WatchKit Extension
+//
+//  Created by Cal Stephens on 10/25/18.
+//  Copyright © 2018 Harshil Shah. All rights reserved.
+//
+
+import WatchKit
+
+class ExtensionDelegate: NSObject, WKExtensionDelegate {
+
+}
---
+++ Examples/watchOS Example/ActivityRings watchOS Example/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ActivityRings watchOS Example
+//
+//  Created by Cal Stephens on 10/25/18.
+//  Copyright © 2018 Harshil Shah. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
+
